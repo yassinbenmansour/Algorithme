725e92149aaa1f2b1fa1786d2467dfe2a6945a8e
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-08-21 16:05:12 +0100

    update

---
+++ Javascript note/notes/array_loop.js
@@ -0,0 +1,13 @@
+// array  loop
+
+var fruit = ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓"]
+
+
+//console.log(fruit[0])
+//console.log(fruit[1])
+//console.log(fruit[2])
+
+
+for (var i = 0; i < fruit.length; i++) {
+  console.log(i, "=>", fruit[i])
+}
