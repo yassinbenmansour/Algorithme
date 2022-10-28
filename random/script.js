1b16856f242145da30cc0acceca9fd813d61088c
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-10-28 16:57:35 +0100

    update

--- random/script.js
+++ random/script.js
@@ -1,6 +1,20 @@
-function RandomFc (){
+function checkCD (){
     let age = document.getElementById("age").value;
 
     let checkvalue = (age > 18) ? "rak kbir" : "rak baqi sghir";
     document.getElementById("msg").innerHTML=checkvalue
 }
+
+
+
+
+
+
+function Randomnbr(){
+     let nbr = Math.floor(Math.random() * 10);
+
+     return nbr ;
+}
+
+
+console.log(Randomnbr());
